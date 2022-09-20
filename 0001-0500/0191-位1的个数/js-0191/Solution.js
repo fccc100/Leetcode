f20e807d3bd3539be4a1a39bb048884f0781880d
朱贤忠 <22525482@qq.com>
2022-09-20 10:40:39 +0800

    0191-位1的个数

---
+++ 0001-0500/0191-位1的个数/js-0191/Solution.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    let res = 0;
+    while (n != 0) {
+        n &= n - 1;
+        res ++;
+    }
+    return res;
+};
