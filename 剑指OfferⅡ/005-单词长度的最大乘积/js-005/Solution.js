294aa121a3d0a3e2095390626b141fe9255699b8
朱贤忠 <22525482@qq.com>
2022-09-20 10:23:37 +0800

    .gitignore清除缓存

---
+++ 剑指OfferⅡ/005-单词长度的最大乘积/js-005/Solution.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var maxProduct = function(words) {
+    let n = words.length;
+    let masks = Array(n).fill(0);
+
+    for (let i = 0; i < n; i++) {
+        let word = words[i];
+
+        for (let j = 0; j < word.length; j++) {
+            masks[i] |= 1 << (word[j].charCodeAt() - 'a'.charCodeAt());
+        }
+    }
+
+    let res = 0;
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if ((masks[i] & masks[j]) == 0) {
+                res = Math.max(res, words[i].length * words[j].length);
+            }
+        }
+    }
+    return res;
+};
