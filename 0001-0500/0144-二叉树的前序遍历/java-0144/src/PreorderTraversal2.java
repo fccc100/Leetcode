615748098f8da0a31a0776185960280fd448186a
zhuxianzhong <22525482@qq.com>
2022-09-21 23:46:17 +0800

    0144-二叉树的前序遍历

---
+++ 0001-0500/0144-二叉树的前序遍历/java-0144/src/PreorderTraversal2.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class PreorderTraversal2 {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public ArrayList<Integer> res;
+    public List<Integer> preorderTraversal(TreeNode root) {
+        res = new ArrayList<>();
+        preorder(root);
+        return res;
+    }
+
+    private void preorder(TreeNode root) {
+        if (root == null) return;
+
+        res.add(root.val);
+        preorder(root.left);
+        preorder(root.right);
+    }
+}
