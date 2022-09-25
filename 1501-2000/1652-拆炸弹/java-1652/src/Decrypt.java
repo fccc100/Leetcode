d0f7bc3f606de57999b98c3bc1a0f8ae1ff57225
zhuxianzhong <22525482@qq.com>
2022-09-25 13:11:56 +0800

    1652-拆炸弹

--- 1501-2000/1652-拆炸弹/java-1652/src/Decrypt.java
+++ 1501-2000/1652-拆炸弹/java-1652/src/Decrypt.java
@@ -1,7 +1,3 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
 
 class Decrypt {
     public int[] decrypt(int[] code, int k) {
@@ -33,76 +29,5 @@ class Decrypt {
             }
         }
         return res;
-    }
-}
-
-class Solution {
-    public int transportationHub(int[][] path) {
-
-        int n = 0;
-        for (int i = 0; i < path.length; i++) {
-            n = Math.max(n, Math.max(path[i][0], path[i][1]));
-        }
-        n = n + 1;
-
-        ArrayList<HashSet<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            graph.add(new HashSet());
-        }
-
-        int [] inDegree = new int[n];
-
-        for (int i = 0; i < path.length; i++) {
-
-            graph.get(path[i][0]).add(path[i][1]);
-            inDegree[path[i][1]]++;
-        }
-        System.out.print(graph);
-
-        int visitedCnt = 0;
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < inDegree.length; i++) {
-            if (inDegree[i] == 0) {
-                queue.offer(i);
-                visitedCnt++;
-            }
-        }
-        if (queue.isEmpty()) return -1;
-
-
-
-        int res = -1;
-        boolean flag = false;
-        while (!queue.isEmpty()) {
-            int len = queue.size();
-
-            for (int i = 0; i < len; i++) {
-                int curV = queue.poll();
-
-                HashSet<Integer> curSet = graph.get(curV);
-                for (Integer key : curSet) {
-                    inDegree[key]--;
-                    if (inDegree[key] == 0) {
-                        queue.offer(key);
-                        if (len == 1 && visitedCnt == n - 1) {
-                            flag = true;
-                            res = key;
-                        }
-                    }
-                }
-            }
-        }
-
-        boolean hasRes = true;
-        for (int i = 0; i < inDegree.length; i++) {
-            if (inDegree[i] > 0) {
-                hasRes = false;
-            }
-        }
-        if (hasRes && false) {
-            return res;
-        } else {
-            return -1;
-        }
     }
 }
