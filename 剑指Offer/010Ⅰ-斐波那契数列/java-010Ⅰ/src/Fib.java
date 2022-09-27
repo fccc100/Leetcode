12a6cd6819c30a535848e0faa4402ccbe04c4a4d
朱贤忠 <22525482@qq.com>
2022-09-27 14:34:14 +0800

    010Ⅰ-斐波那契数列

---
+++ 剑指Offer/010Ⅰ-斐波那契数列/java-010Ⅰ/src/Fib.java
@@ -0,0 +1,15 @@
+class Fib {
+    public int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        int mod = 1000000007;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % mod;
+        }
+        return dp[n] % mod;
+    }
+}
