1cf45c5e4287aa75e8c756e65a0f9ce50a1658bc
朱贤忠 <22525482@qq.com>
2022-09-30 09:39:43 +0800

    01.08-零矩阵

---
+++ 面试题/01.08-零矩阵/java-01.08/src/SetZeroes.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+
+class SetZeroes {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        if (m == 0) return;
+        int n = matrix[0].length;
+        HashSet<Integer> row = new HashSet<>();
+        HashSet<Integer> col = new HashSet<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row.add(i);
+                    col.add(j);
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row.contains(i) || col.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
