fa32bc4183bb12679545c3be095221fdaa69c3b9
zhuxianzhong <22525482@qq.com>
2022-10-04 23:36:11 +0800

    剑指Offer010Ⅱ-青蛙跳台阶问题

---
+++ 剑指Offer/010Ⅱ-青蛙跳台阶问题/java-010Ⅱ/src/NumWays.java
@@ -0,0 +1,16 @@
+class NumWays {
+    public int numWays(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        int MOD = 1000000007;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+        return dp[n];
+    }
+}
