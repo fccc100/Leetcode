af5fb15788ee090bb1bd3f868f53e1b1ce446b81
朱贤忠 <22525482@qq.com>
2022-10-08 17:29:34 +0800

    剑指Offer063-股票的最大利润

---
+++ 剑指Offer/063-股票的最大利润/java-063/src/MaxProfit.java
@@ -0,0 +1,17 @@
+class MaxProfit {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        if (n == 0) return 0;
+
+        int res = 0;
+        int min = prices[0];
+        for (int i = 1; i < n; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            } else {
+                res = Math.max(res, prices[i] - min);
+            }
+        }
+        return res;
+    }
+}
