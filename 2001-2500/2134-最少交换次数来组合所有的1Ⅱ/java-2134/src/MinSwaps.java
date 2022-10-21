ade847bc52edb29a449f85a61d2aff65ff475c42
朱贤忠 <22525482@qq.com>
2022-10-21 17:36:42 +0800

    2134-最少交换次数来组合所有的1Ⅱ

---
+++ 2001-2500/2134-最少交换次数来组合所有的1Ⅱ/java-2134/src/MinSwaps.java
@@ -0,0 +1,35 @@
+class MinSwaps {
+    public int minSwaps(int[] nums) {
+        int n = nums.length;
+
+        int oneCnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 1) {
+                oneCnt++;
+            }
+        }
+
+        int l = 0;
+        int r = oneCnt - 1;
+        int curOneCnt = 0;
+        for (int i = 0; i <= r; i++) {
+            if (nums[i] == 1) {
+                curOneCnt++;
+            }
+        }
+
+        int maxOneCnt = curOneCnt;
+        while (l < n) {
+            r = (r + 1) % n;
+            if (nums[r] == 1) {
+                curOneCnt++;
+            }
+            if (nums[l] == 1) {
+                curOneCnt--;
+            }
+            l++;
+            maxOneCnt = Math.max(maxOneCnt, curOneCnt);
+        }
+        return oneCnt - maxOneCnt;
+    }
+}
