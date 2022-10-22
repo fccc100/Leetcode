1a96bc2d10285f0727dc0d2b27a6e5bcbe20f7a3
zhuxianzhong <22525482@qq.com>
2022-10-22 23:03:48 +0800

    0204-计数质数

---
+++ 0001-0500/0204-计数质数/java-0204/src/CountPrimes2.java
@@ -0,0 +1,22 @@
+public class CountPrimes2 {
+    // TLE
+    public int countPrimes(int n) {
+        int res = 0;
+        for (int i = 1; i < n; i++) {
+            if (isPrime(i)) {
+                res++;
+            }
+        }
+        return res;
+    }
+
+    private boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= n / i; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ 0001-0500/0204-计数质数/java-0204/src/CountPrimes3.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class CountPrimes3 {
+    public int countPrimes(int n) {
+        int res = 0;
+        int[] isPrime = new int[n];
+        Arrays.fill(isPrime, 1);
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i] == 1) {
+                res ++;
+                if ((long) i * i < n) {
+                    for (int j = i * i; j < n; j += i) {
+                        isPrime[j] = 0;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
