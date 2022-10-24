d6a32e74a09976bf7a44aef00b8eb74f990a5cff
朱贤忠 <22525482@qq.com>
2022-10-24 18:25:23 +0800

    2449-使数组相似的最少操作次数

---
+++ 2001-2500/2449-使数组相似的最少操作次数/java-2449/src/MakeSimilar.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+class MakeSimilar {
+    public long makeSimilar(int[] nums, int[] target) {
+        int n = nums.length;
+
+        ArrayList<Integer> odds = new ArrayList<>();
+
+
+        return 0;
+    }
+}
