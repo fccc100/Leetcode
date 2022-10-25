1448cced6bbb299f1b7d4c05ff4f1fb75827adb1
朱贤忠 <22525482@qq.com>
2022-10-25 09:31:20 +0800

    2449-使数组相似的最少操作次数

--- 2001-2500/2449-使数组相似的最少操作次数/java-2449/src/MakeSimilar.java
+++ 2001-2500/2449-使数组相似的最少操作次数/java-2449/src/MakeSimilar.java
@@ -1,12 +1,46 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 class MakeSimilar {
     public long makeSimilar(int[] nums, int[] target) {
         int n = nums.length;
 
-        ArrayList<Integer> odds = new ArrayList<>();
+        ArrayList<Integer> tempNums = new ArrayList<>();
+        ArrayList<Integer> tempTarget = new ArrayList<>();
 
+        for (int i = 0; i < n; i++) {
+            if (nums[i] % 2 == 0) {
+                tempNums.add(nums[i]);
+            }
+            if (target[i] % 2 == 0) {
+                tempTarget.add(target[i]);
+            }
+        }
 
-        return 0;
+        long res = 0;
+        Collections.sort(tempNums);
+        Collections.sort(tempTarget);
+        for (int i = 0; i < tempNums.size(); i++) {
+            res += Math.abs(tempNums.get(i) - tempTarget.get(i));
+        }
+
+        tempNums.clear();
+        tempTarget.clear();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] % 2 == 1) {
+                tempNums.add(nums[i]);
+            }
+            if (target[i] % 2 == 1) {
+                tempTarget.add(target[i]);
+            }
+        }
+
+        Collections.sort(tempNums);
+        Collections.sort(tempTarget);
+        for (int i = 0; i < tempNums.size(); i++) {
+            res += Math.abs(tempNums.get(i) - tempTarget.get(i));
+        }
+
+        return res / 4;
     }
 }
