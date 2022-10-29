5134777c869cc94e677232f2c39ac9ddf8d9e89c
zhuxianzhong <22525482@qq.com>
2022-10-29 22:27:20 +0800

    1773-统计匹配检索规则的物品数量

---
+++ 1501-2000/1773-统计匹配检索规则的物品数量/java-1773/src/CountMatches.java
@@ -0,0 +1,20 @@
+class CountMatches {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int n = items.size();
+
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            if (ruleKey.equals("type") && ruleValue.equals(items.get(i).get(0))) {
+                res++;
+            }
+
+            if (ruleKey.equals("color") && ruleValue.equals(items.get(i).get(1))) {
+                res++;
+            }
+            if (ruleKey.equals("name") && ruleValue.equals(items.get(i).get(2))) {
+                res++;
+            }
+        }
+        return res;
+    }
+}
