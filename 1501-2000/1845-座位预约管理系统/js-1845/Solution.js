545ce22b63bf5f8a3078d3db16a261cbe931ed0f
朱贤忠 <22525482@qq.com>
2022-11-09 15:54:33 +0800

    1845-座位预约管理系统

---
+++ 1501-2000/1845-座位预约管理系统/js-1845/Solution.js
@@ -0,0 +1,108 @@
+/**
+ * @param {number} n
+ */
+var SeatManager = function(n) {
+    this.pq = new PriorityQueue1()
+    for (let i = 1; i <= n; i++) {
+        this.pq.offer(i)
+    }
+};
+
+/**
+ * @return {number}
+ */
+SeatManager.prototype.reserve = function() {
+    return this.pq.poll()
+};
+
+/**
+ * @param {number} seatNumber
+ * @return {void}
+ */
+SeatManager.prototype.unreserve = function(seatNumber) {
+    this.pq.offer(seatNumber)
+};
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * var obj = new SeatManager(n)
+ * var param_1 = obj.reserve()
+ * obj.unreserve(seatNumber)
+ */
+
+class PriorityQueue1 {
+    constructor(comparator = this.__defaultComparator) {
+        this.comparator = comparator
+        this.data = []
+    }
+
+    size() {
+        return this.data.length
+    }
+
+    isEmpty() {
+        return this.size() == 0
+    }
+
+    offer(e) {
+        this.data.push(e)
+        this.__siftUp(this.size() - 1)
+    }
+
+    peek() {
+        return this.data[0]
+    }
+
+    poll() {
+        let res = this.peek()
+        this.__swap(0, this.size() - 1)
+        this.data.pop()
+        this.__siftDown(0)
+        return res
+    }
+
+    __defaultComparator(a, b) {
+        return a - b;
+    }
+
+    __leftChild(index) {
+        return 2 * index + 1
+    }
+
+    __rightChild(index) {
+        return 2 * index + 2
+    }
+
+    __parent(index) {
+        return (index - 1) >> 1
+    }
+
+    __swap(i, j) {
+        let temp = this.data[i]
+        this.data[i] = this.data[j]
+        this.data[j] = temp
+    }
+
+    __siftUp(index) {
+        while (index > 0 && this.comparator(this.data[this.__parent(index)], this.data[index]) > 0) {
+            this.__swap(index, this.__parent(index))
+            index = this.__parent(index)
+        }
+    }
+
+    __siftDown(index) {
+        while (this.__leftChild(index) < this.size()) {
+            let targetIndex = this.__leftChild(index)
+            let rightIndex = this.__rightChild(index)
+            if (rightIndex < this.size() && this.comparator(this.data[rightIndex], this.data[targetIndex]) < 0) {
+                targetIndex = rightIndex
+            }
+            if (this.comparator(this.data[index], this.data[targetIndex]) < 0) {
+                break
+            }
+
+            this.__swap(index, targetIndex)
+            index = targetIndex
+        }
+    }
+}
