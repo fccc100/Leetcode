d20f2be82f91673731a72e62e1f5a8e523ad987f
zhuxianzhong <22525482@qq.com>
2022-11-12 19:51:15 +0800

    2031-1比0多的子数组个数

--- 2001-2500/2031-1比0多的子数组个数/java-2031/src/SubarraysWithMoreZerosThanOnes.java
+++ 2001-2500/2031-1比0多的子数组个数/java-2031/src/SubarraysWithMoreZerosThanOnes.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
 import java.util.TreeSet;
 
 class SubarraysWithMoreZerosThanOnes {
+    int mod = 1000000007;
     public int subarraysWithMoreZerosThanOnes(int[] nums) {
         int n = nums.length;
 
@@ -10,12 +12,64 @@ class SubarraysWithMoreZerosThanOnes {
             presum[i] = presum[i - 1] + (nums[i - 1] == 0 ? -1 : 1);
         }
 
-        TreeSet<Integer> set = new TreeSet<>();
-        for (int v : nums) {
+        reverse(presum);
 
+        return reversePairs(presum);
+    }
+
+    private void reverse(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l < r) {
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
         }
+    }
 
-        return 0;
+    public int reversePairs(int[] nums) {
+        int n = nums.length;
+
+        return mergeSort(nums, 0, n - 1);
+    }
+
+    private int mergeSort(int[] nums, int l, int r) {
+        if (l >= r) return 0;
+        int res = 0;
+
+        int mid = l + (r - l >> 1);
+        res = (res + mergeSort(nums, l, mid)) % mod;
+        res = (res + mergeSort(nums, mid + 1, r)) % mod;
+        if (nums[mid] > nums[mid + 1]) {
+            res = (res + merge(nums, l, mid, r)) % mod;
+        }
+        return res;
+    }
+
+    private int merge(int[] nums, int l, int mid, int r) {
+        int[] temp = Arrays.copyOfRange(nums, l, r + 1);
+        int i = l;
+        int j = mid + 1;
+        int res = 0;
+        for (int k = l; k <= r; k++) {
+            if (i > mid) {
+                nums[k] = temp[j - l];
+                j++;
+            } else if (j > r) {
+                nums[k] = temp[i - l];
+                i++;
+            } else if (temp[i - l] <= temp[j - l]) {
+                nums[k] = temp[i - l];
+                i++;
+            } else {
+                res = (res + mid - i + 1) % mod;
+                nums[k] = temp[j - l];
+                j++;
+            }
+        }
+        return res;
     }
 }
 
