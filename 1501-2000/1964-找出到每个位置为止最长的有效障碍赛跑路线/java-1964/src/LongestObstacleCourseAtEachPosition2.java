b06add04aec5a9057554df67320ea219163bafcd
朱贤忠 <22525482@qq.com>
2022-11-16 16:19:22 +0800

    1964-找出到每个位置为止最长的有效障碍赛跑路线

--- 1501-2000/1964-找出到每个位置为止最长的有效障碍赛跑路线/java-1964/src/LongestObstacleCourseAtEachPosition2.java
+++ 1501-2000/1964-找出到每个位置为止最长的有效障碍赛跑路线/java-1964/src/LongestObstacleCourseAtEachPosition2.java
@@ -2,43 +2,40 @@ import java.util.Arrays;
 
 public class LongestObstacleCourseAtEachPosition2 {
     public int[] longestObstacleCourseAtEachPosition(int[] obstacles) {
-//        int n = obstacles.length;
-//        if (n == 0) {
-//            return new int[]{};
-//        }
-//
-//        // dp[i]表示长度为i的最长递增子序列的末尾元素最小值
-//        int[] dp = new int[n + 1];
-//        int[] res = new int[n];
-//        Arrays.fill(res, 1);
-//        int len = 1;
-//        dp[len] = obstacles[0];
-//
-//        for (int i = 1; i < n; i++) {
-//            if (obstacles[i] > dp[len]) {
-//                len++;
-//                dp[len] = obstacles[i];
-//                res[i] = Math.max(res[i], len);
-//            } else {
-//
-//                // dp数组中[0, len]区间小于nums[i]的最大值索引
-//                int pos = lower(dp, 0, len, obstacles[i]);
-//
-//                dp[pos + 1] = obstacles[i];
-//                res[i] = Math.max(res[i], pos + 1);
-//            }
-//        }
-//        return res;
-        return new int[]{};
+        int n = obstacles.length;
+
+        int[] res = new int[n];
+
+        // dp[i]表示长度为i的序列的最后一个元素的最小值
+        int[] dp = new int[n + 1];
+        int len = 1;
+        dp[len] = obstacles[0];
+        res[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (obstacles[i] >= dp[len]) {
+                len++;
+                dp[len] = obstacles[i];
+                res[i] = len;
+            } else {
+                // 找到打于obstacles[i]的最小值的索引
+                int insertPos = lower(dp, 0, len, obstacles[i]);
+
+                dp[insertPos] = obstacles[i];
+                res[i] = insertPos;
+            }
+        }
+
+        return res;
     }
 
     private int lower(int[] nums, int l, int r, int target) {
         while (l < r) {
-            int mid = l + (r - l + 1 >> 1);
-            if (nums[mid] < target) {
-                l = mid;
+            int mid = l + (r - l >> 1);
+            if (nums[mid] <= target) {
+                l = mid + 1;
             } else {
-                r = mid - 1;
+                r = mid;
             }
         }
         return l;
