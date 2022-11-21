50e10e6d0a56841abc58e4175fa5e89512189a2a
朱贤忠 <22525482@qq.com>
2022-11-21 17:42:31 +0800

    2111-使数组K递增的最少操作次数

--- 2001-2500/2111-使数组K递增的最少操作次数/java-2111/src/KIncreasing.java
+++ 2001-2500/2111-使数组K递增的最少操作次数/java-2111/src/KIncreasing.java
@@ -1,33 +1,63 @@
+import java.util.ArrayList;
+
 class KIncreasing {
     public int kIncreasing(int[] arr, int k) {
         int n = arr.length;
 
-        int l = 0;
-        int r = n;
-        while (l < r) {
-            int mid = l + (r - l >> 1);
+        int res = 0;
+        for (int i = 0; i < k; i++) {
+            ArrayList<Integer> cur = new ArrayList<>();
+            for (int j = i; j < n; j += k) {
+                cur.add(arr[j]);
+            }
 
-            if (check(arr, mid, k)) {
-                r = mid;
+            int[] nums = new int[cur.size()];
+            int idx = 0;
+            for (Integer num : cur) {
+                nums[idx] = num;
+                idx++;
+            }
+
+            res += nums.length - lengthOfLIS(nums);
+        }
+        return res;
+    }
+
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+
+        // dp[i]表示长度为i的最长递增子序列的末尾元素最小值
+        int[] dp = new int[n + 1];
+        int len = 1;
+        dp[len] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] >= dp[len]) {
+                len++;
+                dp[len] = nums[i];
             } else {
-                l = mid + 1;
+
+                // dp数组中[0, len]区间小于nums[i]的最大值索引
+                int pos = lower(dp, 0, len, nums[i]);
+
+                dp[pos + 1] = nums[i];
+            }
+        }
+        return len;
+    }
+
+    private int lower(int[] nums, int l, int r, int target) {
+        while (l < r) {
+            int mid = l + (r - l + 1 >> 1);
+            if (nums[mid] <= target) {
+                l = mid;
+            } else {
+                r = mid - 1;
             }
         }
         return l;
     }
-
-    private boolean check(int[] arr, int c, int k) {
-        int n = arr.length;
-
-        int cnt = 0;
-        for (int i = 0; i < n - k; i++) {
-            if (arr[i] > arr[i + k]) cnt++;
-        }
-        return cnt <= c;
-    }
 }
-//[12,6,12,6,14,2,13,17,3,8,11,7,4,11,18,8,8,3] 1
-// 5  6 6  6 2  2 13 3
-
-//[12,6,12,6,14,2,13,17,3,8,11,7,4,11,18,8,8,3]
-//                      996       997  998        999    1000
