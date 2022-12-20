29446e201d2eeb0ee72eac6f32d6b81d2998ebaf
朱贤忠 <22525482@qq.com>
2022-12-20 09:53:42 +0800

    0112-路径总和

---
+++ 0001-0500/0112-路径总和/java-0112/src/HasPathSum.java
@@ -0,0 +1,12 @@
+class HasPathSum {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
