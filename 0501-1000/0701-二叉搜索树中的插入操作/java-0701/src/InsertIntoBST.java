338163a4d843b855e8a29a6497d09dd8ff984b69
朱贤忠 <22525482@qq.com>
2022-12-21 17:49:59 +0800

    0701-二叉搜索树中的插入操作

---
+++ 0501-1000/0701-二叉搜索树中的插入操作/java-0701/src/InsertIntoBST.java
@@ -0,0 +1,12 @@
+class InsertIntoBST {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+
+        if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        } else {
+            root.left = insertIntoBST(root.left, val);
+        }
+        return root;
+    }
+}
