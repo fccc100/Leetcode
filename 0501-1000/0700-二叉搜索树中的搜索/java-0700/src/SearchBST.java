39c6209843cf238a420ca64d948a2a0055dfc972
朱贤忠 <22525482@qq.com>
2022-12-21 17:44:10 +0800

    0700-二叉搜索树中的搜索

---
+++ 0501-1000/0700-二叉搜索树中的搜索/java-0700/src/SearchBST.java
@@ -0,0 +1,12 @@
+class SearchBST {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) return null;
+        if (root.val == val) return root;
+
+        if (root.val < val) {
+            return searchBST(root.right, val);
+        } else {
+            return searchBST(root.left, val);
+        }
+    }
+}
