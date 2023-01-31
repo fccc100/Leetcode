b6698aea4341f86929c0c0619c606056936bf4eb
朱贤忠 <22525482@qq.com>
2023-01-31 09:40:27 +0800

    2319-判断矩阵是否是一个X矩阵

---
+++ 2001-2500/2319-判断矩阵是否是一个X矩阵/java-2319/src/CheckXMatrix.java
@@ -0,0 +1,15 @@
+class CheckXMatrix {
+    public boolean checkXMatrix(int[][] grid) {
+        int n = grid.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || j == n - 1 - i) {
+                    if (grid[i][j] == 0) return false;
+                } else {
+                    if (grid[i][j] != 0) return false;
+                }
+            }
+        }
+        return true;
+    }
+}
