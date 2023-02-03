eb74341dece7f5d74f5df1dee45ab5fbd547be50
朱贤忠 <22525482@qq.com>
2023-02-03 15:23:17 +0800

    2176-统计数组中相等且可以被整除的数对

---
+++ 2001-2500/2176-统计数组中相等且可以被整除的数对/java-2176/src/CountPairs.java
@@ -0,0 +1,12 @@
+class CountPairs {
+    public int countPairs(int[] nums, int k) {
+        int n = nums.length;
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == nums[j] && (i * j) % k == 0) res++;
+            }
+        }
+        return res;
+    }
+}
