c29dbc72edcbd7066c0585929a19a944fc1a305e
zhuxianzhong <22525482@qq.com>
2023-02-26 18:44:01 +0800

    2571-将整数减少到零需要的最少操作数

---
+++ 2501-3000/2571-将整数减少到零需要的最少操作数/java-2571/src/MinOperations.java
@@ -0,0 +1,9 @@
+class MinOperations {
+    public int minOperations(int n) {
+        if ((n & (n - 1)) == 0) {
+            return 1;
+        }
+
+        return Math.min(minOperations(n & (n - 1)) + 1, minOperations(n + (n & (-n))) + 1);
+    }
+}
