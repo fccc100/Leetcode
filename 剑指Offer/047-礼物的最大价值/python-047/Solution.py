e3c86799ac55255e60763f6954aee1fc14bfea3a
朱贤忠 <22525482@qq.com>
2023-03-08 09:58:46 +0800

    剑指Offer047-礼物的最大价值

---
+++ 剑指Offer/047-礼物的最大价值/python-047/Solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxValue(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        dp = [[0] * (n + 1)] * (m + 1)
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + grid[i - 1][j - 1]
+        return dp[m][n]
