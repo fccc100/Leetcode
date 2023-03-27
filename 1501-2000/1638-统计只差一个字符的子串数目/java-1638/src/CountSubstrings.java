aff7220c6eabd5cf2e70a29266d2407d0fbefe28
朱贤忠 <22525482@qq.com>
2023-03-27 10:26:31 +0800

    1638-统计只差一个字符的子串数目

---
+++ 1501-2000/1638-统计只差一个字符的子串数目/java-1638/src/CountSubstrings.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class CountSubstrings {
+    public int countSubstrings(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+        int res = 0;
+        for (int i = 1; i <= Math.min(m, n); i++) {
+            // 长度为i的字串
+            for (int j = 0; j <= m - i; j++) {
+                for (int k = 0; k <= n - i; k++) {
+                    if (check(s, j, t, k, i)) {
+                        System.out.println(j);
+                        System.out.println(k);
+                        System.out.println(i);
+                        System.out.println("---");
+                        res++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+    
+    private boolean check(String s, int j, String t, int k, int len) {
+        int res = 0;
+        while (len > 0) {
+            if (s.charAt(j) != t.charAt(k)) {
+                res++;
+            }
+            j++;
+            k++;
+            len--;
+        }
+        return res == 1;
+    }
+}
