d6999d2fa64fe040e0adbf3ed1d5f5507536ea4c
朱贤忠 <22525482@qq.com>
2023-03-29 11:07:23 +0800

    1641-统计字典序元音字符串的数目

---
+++ 1501-2000/1641-统计字典序元音字符串的数目/python-1641/Solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countVowelStrings(self, n: int) -> int:
+        a, e, i, o, u = 1, 1, 1, 1, 1
+        while n > 1:
+            a = a + e + i + o + u
+            e = e + i + o + u
+            i = i + o + u
+            o = o + u
+            u = u
+            n -= 1
+        return a + e + i + o + u
+        
